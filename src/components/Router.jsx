79278dedc89500ebd7dd0d1f6b296e32888cd847
Dantaslindoo <a27958@alunos.ipca.pt>
2025-05-13 11:28:28 +0100

    organizacao de pastas2

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import LogIn from '../LogIn';
-import SignUp from '../SignUp';
+import LogIn from '../pages/LogIn';
+import SignUp from '../pages/SignUp';
+import ForgotPassword from '../pages/ForgotPassword';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} /> {/* Vai diretamente para o login, a raiz */}
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<LogIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/password" element={<ForgotPassword />} />
       </Routes>
     </Router>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './Login.css'
 import './Signup.css'
-import LogIn from './LogIn.jsx'
-import SignUp from './SignUp.jsx'
+import LogIn from './pages/LogIn.jsx'
+import SignUp from './pages/SignUp.jsx'
 import App from './components/Router.jsx';
 
 createRoot(document.getElementById('root')).render(
